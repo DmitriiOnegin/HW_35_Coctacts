bbcaba4a523c16edb973b6d054da7f215b094ebc
Dmitrii Onegin <dimimimo@mail.ru>
2022-03-01 13:36:45 +0300

    Initial Commit

---
+++ HW_35_Coctacts/HW_35_CoctactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_35_CoctactsApp.swift
+//  HW_35_Coctacts
+//
+//  Created by Dmitrii Onegin on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_35_CoctactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
